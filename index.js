d56186995d7790290d433dd28a8703acf3d34a82
cr7cr8 <cr7cr8@126.com>
2019-12-03 15:50:24 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const bcrypt = require("bcryptjs")
+
+let count =0
+
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(`Hello World! 
+    ${count}
+    ${new Date().toLocaleString("cn-CN", { timeZone: 'Asia/Shanghai' }) }
+    `);
+    res.write(bcrypt.hashSync(Date.now().toString(),10))
+    res.end();
+    count++;
+  }).listen(process.env.PORT || 80);
